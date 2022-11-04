39911f93104a11a035e27cc957c12f5a6d7b6ce7
do1e <dpj.email@qq.com>
2022-11-04 21:52:07 +0800

    v1.0.1 add ConnectionError

--- NJUlogin/utils/get_post.py
+++ NJUlogin/utils/get_post.py
@@ -7,9 +7,13 @@ def get(session: requests.Session, url: str) -> requests.Response:
         return session.get(url, **config.getkwargs)
     except requests.exceptions.Timeout:
         raise TimeoutError('请求网页"%s"超时' % url)
+    except requests.exceptions.ConnectionError:
+        raise ConnectionError('请求网页"%s"时发生连接错误，请检查网络连接' % url)
 
 def post(session: requests.Session, url: str, data: dict) -> requests.Response:
     try:
         return session.post(url, data=data, **config.getkwargs)
     except requests.exceptions.Timeout:
         raise TimeoutError('向网页"%s"提交数据超时' % url)
+    except requests.exceptions.ConnectionError:
+        raise ConnectionError('向网页"%s"提交数据时发生连接错误，请检查网络连接' % url)
