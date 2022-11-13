a051dcbbe618357d899b8c36be8d14fd57d5dd43
do1e <dpj.email@qq.com>
2022-11-13 14:58:28 +0800

    v2.3.5 fix some bug in pwdLogin

--- NJUlogin/pwdLogin.py
+++ NJUlogin/pwdLogin.py
@@ -28,8 +28,8 @@ class pwdLogin(baseLogin):
         mobileLogin: bool, 是否是APP登录
         -------
         """
-        if mobileLogin:
-            assert parse(headers['User-Agent']).is_mobile, 'mobileLogin要求使用手机User-Agent'
+        if mobileLogin and not parse(headers['User-Agent']).is_mobile:
+            print('WARNING: mobileLogin要求使用手机User-Agent，当然你也可以不这样做，但有一定的风险')
         super().__init__(headers)
         self.username = username
         self.password = password
@@ -85,18 +85,14 @@ class pwdLogin(baseLogin):
             # 登录失败
             # 可能登录失败也可能是验证码错误
             selector = etree.HTML(res.text)
-            errorMsg = selector.xpath('//span[@id="errorMsg"]/text()')[0]
-            if errorMsg == '无效的验证码':
-                # 验证码错误
-                return self.login(dest)
-            elif errorMsg == '您提供的用户名或者密码有误':
-                # 账号密码错误
-                print('登录失败，账号密码错误')
-                return None
-            else:
+            try:
+                errorMsg = selector.xpath('//span[@id="msg"]/text()')[0]
+            except IndexError:
+                errorMsg = '需要输入验证码'
                 # 需要输入动态码
-                res = self.post(self.session, urls.dynamicCode, timeout=self.getTimeout,
-                    data={'username': self.username, 'authCodeTypeName': 'reAuthDynamicCodeType'}
+                res = self.post(urls.dynamicCode,
+                    data={'username': self.username, 'authCodeTypeName': 'reAuthDynamicCodeType'},
+                    timeout=self.getTimeout
                 )
                 msg = 'fail'
                 try:
@@ -119,10 +115,23 @@ class pwdLogin(baseLogin):
                     'execution': selector.xpath('//input[@name="execution"]/@value')[0],
                     '_eventId': selector.xpath('//input[@name="_eventId"]/@value')[0]
                 }
-                res = self.post(self.session, url, data=data, timeout=self.getTimeout)
+                res = self.post(url, data=data, timeout=self.getTimeout)
                 if res.url == url:
                     print('登录失败，动态码错误')
                     return None
+                else:
+                    print('登录成功')
+                    return self.session
+            if errorMsg == '无效的验证码':
+                # 验证码错误
+                return self.login(dest)
+            elif errorMsg == '您提供的用户名或者密码有误':
+                # 账号密码错误
+                print('登录失败，账号密码错误')
+                return None
+            else:
+                print('登录失败，%s' % errorMsg)
+                return None
 
         print('登录成功')
         return self.session
